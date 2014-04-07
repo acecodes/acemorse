8fe690a8fb5f3dd43fc946c412fc02f6188ff086
Ace <real.deal.ace@gmail.com>
2014-04-07 00:47:52 -0700

    Initial commit
    Boom.

---
+++ MorseCodeEngine.py
@@ -0,0 +1,54 @@
+# Morse Code Generator/Translator
+# Functions for creating or translating Morse code messages.
+
+alphabet = {'0':'-----', '1':'.----', '2':'..---', '3':'...--',
+'4':'....-','5':'.....','6':'-....','7':'--...','8':'---..','9':'----.', 'a':'.-', 'b':'-...', 'c':'-.-.', 'd':'-..', 'e':'.', 'f':'..-.','g':'--.','h':'....',
+'i':'..','j':'.---','k':'-.-','l':'.-..','m':'--','n':'-.','o':'---','p':'.--.','q':'--.-','r':'.-.',
+'s':'...','t':'-','u':'..-','v':'..-','w':'.--','y':'-.--','z':'--..'}
+
+punctuation = ['!', '?', '\'', ';', ','] # A list of punctuation marks
+
+alpha_list = [] # Empty list that will hold only the alphanumeric characters from the alphabet dictionary
+code_list = [] # Empty list that will hodl only the Morse code from the alphabet dictionary
+
+# Looping through the dictionary to separate the alphanumerics and Morse code
+for k, v in sorted(alphabet.items()): 
+	alpha_list.append(k)
+	code_list.append(v)
+
+# Generates Morse code list from an English message
+# The 'plain' flag is for messages that will not be turned into lists for use by 'MorseTranslate'
+def MorseGenerate(text, plain=False):
+	text = text.lower() # Make everything lower case, as case doesn't matter in Morse
+	morse = [] # Create the list that will eventually hold the Morse code
+	for letter in text: # Search the message for its match in Morse
+		if letter in alphabet:
+			morse.append(alphabet[letter])
+		if letter == ' ' or letter in punctuation: # Attach punctuation or spaces as needed (periods are left out because . is 'e' in Morse)
+			morse.append(letter)
+	if plain != False:
+		return ' '.join(morse)
+	return morse
+
+# Translates a message in Morse code into English
+def MorseTranslate(morse):
+	morse = list(morse) # Create a list out of the entered Morse code
+	english = [] # Empty list for translation
+	for code in morse: # Search the message for its match in English
+		if code in code_list:
+			x = code_list.index(code)
+			english.append(alpha_list[x])
+		if code == ' ' or code in punctuation: # Attach punctuation or spaces as needed
+			english.append(code)
+	return ''.join(english)
+
+# Demonstrations
+if __name__ == '__main__':
+	message = MorseGenerate('Hello World!')
+	message2 = MorseGenerate('Hey pal, let\'s hang out!')
+	message3 = MorseGenerate('This message will not be translated by the function here!', plain=1)
+	print(message)
+	print(MorseTranslate(message))
+	print(message2)
+	print(MorseTranslate(message2))
+	print(message3)
---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>Morse Code Engine</h1>
+
+<p>This script can do two things: generate Morse code or translate it. It's in basic form right now, but once I finish off a few other projects I hope to come back and refine it further (primarily with a GUI and perhaps a web version).</p>
